f9c90e4109ee775a3bfc849dc4ed9b9cf71963c8
john-lenci-bianchi <john.lenci.bianchi@gmail.com>
2023-09-06 02:59:02 +0200

    cleaned done for half of the dataframe

--- cleanUrls.py
+++ cleanUrls.py
@@ -1,7 +1,7 @@
 import pandas as pd
 import tldextract
 
-df = pd.read_csv('dataframeWithLinks.csv', encoding='utf8')
+df = pd.read_csv('dataframeWithLinksClean.csv', encoding='utf8')
 
 # Funzione per estrarre il dominio da un URL
 def extract_domain(url):
@@ -18,9 +18,10 @@ df.drop(columns=["Extracted_Domain"], inplace=True)
 # Ora df contiene solo le righe con URL che fanno riferimento alla testata giornalistica corretta
 print(df)
 # Filtrare il dataframe per escludere le righe con "author" o "category" nella colonna "Url"
-df = df[~df["Url"].str.contains("author|category|@|.jpg|.png|.jpeg|.pdf|.zip|.gif|.mp4|/tag/")]
-
+df = df[~df["Url"].str.contains("author|category|@|.jpg|.png|.jpeg|.pdf|.zip|.gif|.mp4|/tag/|podcast|.fr/|subscribe|page|/feed/|/shop/|/tags/|/newsletters|/topics/|advertise|terms|conditions|cookie|cookies|privacy|policy|about-us|about_us|contact-us|contact_us|contact|subscriptions|donations|email|login|log-in|video|newsletter|/faq/|sitemap|about|main-content|disclaimer|account|signup|,")]
+# Rimuovere le righe duplicate basate sulla colonna 'Url' e conservare solo la prima occorrenza
+df = df.drop_duplicates(subset='Url', keep='first')
 
 df.to_csv('dataframeWithLinksClean.csv', index=False)
 
-print('Done')
+print('done')
--- scriptino.py
+++ scriptino.py
@@ -1,8 +1,27 @@
 import pandas as pd
-df = pd.read_csv('dataframeWithLinksClean.csv', encoding='utf8')
+df = pd.read_csv('contare.csv', encoding='utf8')
 
-count = df['Domain'].unique()
+# Ottenere i valori distinti dalla colonna "Domain" e il loro "Topics" associato
+distinct_domains = df['Domain'].unique()
+Political = 0
+Conspiracy = 0
+Sports = 0
+Health = 0
+for domain in distinct_domains:
+    topics_for_domain = df[df['Domain'] == domain]['Topics'].unique()
+    print(f'Domain: {domain}')
+    print(f'Topics: {", ".join(topics_for_domain)}')
+    print()
+    if (topics_for_domain =="Political news or commentary" ):
+        Political = Political + 1
+    if (topics_for_domain == "Conspiracy theories or hoaxes"):
+        Conspiracy = Conspiracy + 1
+    if (topics_for_domain == "Sports and athletics"):
+        Sports = Sports + 1
+    if (topics_for_domain == "Health or medical information"):
+        Health = Health + 1
 
-print (len(count))
-
-print(len(df))
+print ("Political" + str(Political))
+print ("Conspiracy" + str(Conspiracy))
+print ("Healt" + str(Health))
+print ("Sport" + str(Sports))
